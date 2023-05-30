bb959ba8c109e3aba9696c86893316bdfc05b399
DiYaZeN <minolamarakoon@gmail.com>
2023-05-30 22:06:11 +0530

    database

--- src/employee/employee.resolver.ts
+++ src/employee/employee.resolver.ts
@@ -2,16 +2,18 @@
 import { Query } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
 import { Employee } from './entities/employee.entity';
+import { EmployeeService } from './employee.service';
+// import { InjectRepository } from '@nestjs/typeorm'; 
 
 @Resolver(() => Employee)
 export class EmployeeResolver {
 
-    // constructor (private employeeService: EmployeeService)
+    constructor (private employeeService: EmployeeService){}
 
-    // @Query(() => [Employee])
-    // findAll() {
-
-    // }
+    @Query(() => [Employee,{name:"getAllEmployees"}])
+    findAll() {
+        return this.employeeService.findAll();
+    }
 
 
 
