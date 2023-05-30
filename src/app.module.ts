f424496c4338d1398ab0bd20e7517f4f4b192884
DiYaZeN <minolamarakoon@gmail.com>
2023-05-30 22:07:58 +0530

    database2

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,23 +8,47 @@ import { EmployeeModule } from './employee/employee.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+
+
 @Module({
-  imports: [EmployeeModule,GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,   
+  imports: [EmployeeModule, GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
   }), TypeOrmModule.forRoot({
     type : 'postgres',
     host : 'localhost',
     port : 5432,
-    username : 'root',
+    username : 'postgres',
     password : 'root',
     database : 'employee',
     entities : ['dist/**/*.entity{.ts,.js}'],
-  })],
- 
+    logging : true,
+    synchronize : true,
+  })
+  ],
   controllers: [],
   providers: [],
-  // controllers: [AppController],
-  // providers: [AppService],
+  
 })
-export class AppModule {}
+
+
+// @Module({
+//   imports: [EmployeeModule,GraphQLModule.forRoot<ApolloDriverConfig>({
+//     driver: ApolloDriver,   
+//     autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
+//   }), TypeOrmModule.forRoot({
+//     type : 'postgres',
+//     host : 'localhost',
+//     port : 5432,
+//     username : 'postgres',
+//     password : 'root',
+//     database : 'employee',
+//     entities : ['dist/**/*.entity{.ts,.js}'],
+//   })],
+
+//   controllers: [],
+//   providers: [],
+//   // controllers: [AppController],
+//   // providers: [AppService],
+// })
+export class AppModule { }
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -2,16 +2,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './entities/employee.entity';
+import { Repository } from 'typeorm';
+// import { find } from 'rxjs';
 
 @Injectable()
 export class EmployeeService {
-    constructor(@InjectRepository(Employee) private employeeRepository: EmployeeRepository){
+    constructor(@InjectRepository(Employee) private employeeRepository: Repository<Employee>){
         
     }
 
     async findAll(): Promise<Employee[]>{
         return this.employeeRepository.find();
     }
+    
 
 
 
